274bff07f9517c08e842eb12648613b9b09382d2
FIONA MAHINDA <fionamahinda@gmail.com>
2022-11-10 21:59:14 +0300

    First Commit

---
+++ lab0-ex1.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+print("Hello Again")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+
+def main():
+    print("Hello World!")
+    print("Hello Again")
+    print('Yay! Printing.')
+    print("I'd much rather you 'not'")
+    print('I "said" do not touch this')
+main()
